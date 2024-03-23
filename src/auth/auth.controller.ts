9c22bcafa1792f93adfe6c913714265063dad154
viet34tqc <viet34tqc@gmail.com>
2024-03-23 22:58:25 +0700

    Update prisma modal and add validation to add user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import AuthService from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() dto: any) {
-    return this.authService.login();
+  login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 
   @Post('signup')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 class AuthService {
-  login() {
-    return 'login';
+  constructor(private readonly prisma: PrismaService) {}
+  async login(dto: AuthDto) {
+    // generate hashed password
+    const hashedPassword = await argon2.hash(dto.password);
+    // save user with hased password into db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hashedPassword,
+      },
+    });
+
+    delete user.hash;
+
+    // return newly saved user
+    return user;
   }
 
   signup() {
