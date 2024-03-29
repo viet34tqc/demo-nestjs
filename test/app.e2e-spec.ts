9321459dc6b044638058dc1c0618bcc9ea47d224
viet34tqc <viet34tqc@gmail.com>
2024-03-29 11:51:34 +0700

    Fix package

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test } from '@nest/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -8,7 +8,7 @@ describe('should pass', () => {
   let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      modules: [AppModule],
+      imports: [AppModule],
     }).compile();
     const app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
