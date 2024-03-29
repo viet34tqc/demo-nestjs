1c32f3900c063bffd15af6fdd8206acddf7e1299
viet34tqc <viet34tqc@gmail.com>
2024-03-29 11:04:02 +0700

    Clean db before each test

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+  }
+
+  cleanDb() {
+    this.$transaction([this.user.deleteMany(), this.bookmark.deleteMany()]);
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nest/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('should pass', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       modules: [AppModule],
@@ -11,6 +13,9 @@ describe('should pass', () => {
     const app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(async () => {
