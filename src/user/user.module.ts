3719575539bf9d31ac38fc32e5e30dc65d4a5b78
viet34tqc <viet34tqc@gmail.com>
2024-04-24 23:14:59 +0700

    Demo middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserMiddleware } from './user.middleware';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Apply middleware for all routes of 'user'
+    consumer.apply(UserMiddleware).forRoutes(UserController);
+    consumer.apply(UserMiddleware).forRoutes('user');
+    // Only apply middleware for GET method of 'user' route
+    consumer.apply(UserMiddleware).forRoutes({
+      path: 'user',
+      method: RequestMethod.GET,
+    });
+  }
+}
